7f432dfe81837dfd76f58a4aecdba37652784265
Vasile D <vvdurlesteanu@gmail.com>
2023-03-16 16:02:11 +0200

    section React JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const title = 'Vasile';
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const welcome = {
+  title: 'React',
+  greeting: 'Hey'
 }
 
-export default App
+// const randomArray = [
+//   'React', 'Angular', 'Vue'
+// ];
+
+function getTitle(titulo) {
+  return titulo;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {title} </h1>
+      <h1>Hello {welcome.greeting} {welcome.title}</h1>
+      <h1>Hello {getTitle('Reactivity')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"></input>
+      
+    </div>
+  );
+}
+
+export default App;
